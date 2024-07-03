630e6cf8fd66d151adb2ecf78e7fbea6bf62f791
Dan Andrus <danieleandrus@gmail.com>
2024-07-03 12:34:16 +0200

    Fix shader problems for mobile

--- shaders/background.glsl
+++ shaders/background.glsl
@@ -10,15 +10,15 @@ vec4 effect(vec4 color, sampler2D t, vec2 texture_coords, vec2 screen_coords) {
 
     vec4 pixel = texture(t, texture_coords);
     vec4 tint = vec4(1.0, 1.0, 1.0, 1.0);
-    if (screen_coords.y < sin((screen_coords.x + time * 20) / 20) * 15 + 100 * s) {
+    if (screen_coords.y < sin((screen_coords.x + time * 20.0) / 20.0) * 15.0 + 100.0 * s) {
         tint = topColor;
-    } else if (screen_coords.y < sin((screen_coords.x + 20 - time * 20) / 20) * 15 + 200 * s) {
+    } else if (screen_coords.y < sin((screen_coords.x + 20.0 - time * 20.0) / 20.0) * 15.0 + 200.0 * s) {
         tint = mix(topColor, bottomColor, 0.2);
-    } else if (screen_coords.y < sin((screen_coords.x + 60 + time * 20) / 20) * 15 + 300 * s) {
+    } else if (screen_coords.y < sin((screen_coords.x + 60.0 + time * 20.0) / 20.0) * 15.0 + 300.0 * s) {
         tint = mix(topColor, bottomColor, 0.4);
-    } else if (screen_coords.y < sin((screen_coords.x + 90 - time * 20) / 20) * 15 + 400 * s) {
+    } else if (screen_coords.y < sin((screen_coords.x + 90.0 - time * 20.0) / 20.0) * 15.0 + 400.0 * s) {
         tint = mix(topColor, bottomColor, 0.6);
-    } else if (screen_coords.y < sin((screen_coords.x + 50 + time * 20) / 20) * 15 + 500 * s) {
+    } else if (screen_coords.y < sin((screen_coords.x + 50.0 + time * 20.0) / 20.0) * 15.0 + 500.0 * s) {
         tint*= mix(topColor, bottomColor, 0.8);
     } else {
         tint = bottomColor;
